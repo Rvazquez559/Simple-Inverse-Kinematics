6edeee233cefbea797d4edb3debd36dd498d04fb
Rvazquez559 <Rvazquez@gswiring.com>
2024-08-15 08:07:55 -0500

    added license description

--- src/IKKJ.cpp
+++ src/IKKJ.cpp
@@ -1,3 +1,22 @@
+/* 
+ * This file is part of the distribution (https://github.com/Rvazquez559/Simple-Inverse-Kinematics).
+ * Copyright (c) 2024 Roberto Vazquez.
+ * 
+ * This program is free software: you can redistribute it and/or modify  
+ * it under the terms of the GNU General Public License as published by  
+ * the Free Software Foundation, version 3.
+ *
+ * This program is distributed in the hope that it will be useful, but 
+ * WITHOUT ANY WARRANTY; without even the implied warranty of 
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU 
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License 
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+
+
 #include "IKKJ.h"
 
 IKKJ::IKKJ(float  link1,  float link2,  float baseHeight){
